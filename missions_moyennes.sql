dca2ac9b5fec00896a37ac10b7708b69ca443ed6
MegaMawilite <loufoxybun@gmail.com>
2022-11-22 17:03:01 +0100

    mission 2: mmtj04, 05, 21

--- missions_moyennes.sql
+++ missions_moyennes.sql
@@ -24,3 +24,29 @@ INNER JOIN `equipe`
     ON transaction.nomEquipe = equipe.nom
 WHERE style = 'market making'
     AND date LIKE '2021-%'
+
+--04
+SELECT lieu, AVG(prix)
+FROM `transaction`
+INNER JOIN `equipe`
+    ON transaction.nomEquipe = equipe.nom
+WHERE style = 'market making'
+GROUP BY lieu
+
+--05
+SELECT classe_actif
+FROM `trader`
+INNER JOIN `equipe`
+    ON trader.nomEquipe = equipe.nom
+INNER JOIN `transaction`
+    ON transaction.nomEquipe = equipe.nom
+WHERE chef = 'Smith'
+    AND date = '2016-01-01'
+
+--21
+SELECT AVG(anneeExperience)
+FROM `trader`
+INNER JOIN `equipe`
+    ON trader.nomEquipe = equipe.nom
+WHERE classe_actif = 'action'
+GROUP BY style
